5dfb718c54ce8923d05560b3716f459d7552b899
Timothy Rehenel <timothy.rehenel@gmail.com>
2025-04-10 23:46:17 +0300

    feat: migrated fs storage provider to glob package

--- src/storage/fs-storage-provider.ts
+++ src/storage/fs-storage-provider.ts
@@ -1,7 +1,8 @@
-import {DirectoryContent, DirectoryContentDeep, IStorageProvider} from "./types.js";
+import {IStorageProvider} from "./types.js";
 import {File} from "./file.js";
 import fs from "fs";
 import path from "node:path";
+import {glob} from "glob";
 
 export class FsStorageProvider implements IStorageProvider {
     constructor() {
@@ -40,59 +41,15 @@ export class FsStorageProvider implements IStorageProvider {
         return new File(to);
     }
 
-    async readDirectory(dirPath: string): Promise<DirectoryContent> {
-        const items = await fs.promises.readdir(dirPath);
-
-        const result: DirectoryContent = {
-            path: dirPath,
-            dirs: [],
-            files: []
-        };
-
-        for (const itemPath of items) {
-            const fullPath = path.resolve(path.join(dirPath, itemPath));
-            const stats = await fs.promises.stat(fullPath);
-            if (stats.isDirectory()) {
-                result.dirs.push(fullPath);
-            } else if (stats.isFile()) {
-                result.files.push(fullPath);
-            }
-        }
-
-        return result;
+    async readDir(dirPath: string, ignore: string[] = []): Promise<string[]> {
+        return await glob.glob("*", {cwd: dirPath, ignore, noext: false});
     }
 
-    async readDirectoryDeep(dirPath: string, limit: number = 0, level: number = 1): Promise<DirectoryContentDeep | DirectoryContent> {
-        if (limit == 1) {
-            return this.readDirectory(dirPath);
-        }
-
-        const items = await fs.promises.readdir(dirPath);
-
-        const result: DirectoryContentDeep = {
-            path: dirPath,
-            dirs: [],
-            files: []
-        };
-
-        for (const itemPath of items) {
-            const fullPath = path.resolve(path.join(dirPath, itemPath));
-            const stats = await fs.promises.stat(fullPath);
-            if (stats.isDirectory()) {
-                if ((limit == 0 || level < limit - 1)) {
-                    result.dirs.push(await this.readDirectoryDeep(fullPath, limit, level + 1));
-                } else {
-                    result.dirs.push(await this.readDirectory(fullPath));
-                }
-            } else if (stats.isFile()) {
-                result.files.push(fullPath);
-            }
-        }
-
-        return result;
+    async readDirDeep(dirPath: string, ignore: string[] = []): Promise<string[]> {
+        return await glob.glob("**/*", {cwd: dirPath, ignore, noext: false});
     }
 
-    async createDirectory(dirPath: string): Promise<string> {
+    async createDir(dirPath: string): Promise<string> {
         const resolvedPath = path.resolve(dirPath);
 
         await fs.promises.mkdir(resolvedPath, {recursive: true});
@@ -100,7 +57,7 @@ export class FsStorageProvider implements IStorageProvider {
         return resolvedPath;
     }
 
-    async deleteFileOrDirectory(path: string): Promise<void> {
+    async deleteFileOrDir(path: string): Promise<void> {
         await fs.promises.rm(path, {recursive: true});
     }
 
--- src/storage/types.ts
+++ src/storage/types.ts
@@ -11,25 +11,13 @@ export interface IFile {
 
 }
 
-export interface DirectoryContent {
-    path: string;
-    dirs: string[];
-    files: string[];
-}
-
-export interface DirectoryContentDeep {
-    path: string;
-    dirs: (DirectoryContentDeep | DirectoryContent)[];
-    files: string[];
-}
-
 export interface IStorageProvider {
     readFile: (filePath: string) => Promise<IFile>;
     createFile: (filePath: string, content: Buffer) => Promise<IFile>;
     copyFile: (filePath: string, targetPath: string) => Promise<IFile>;
     moveFile: (filePath: string, targetPath: string) => Promise<IFile>;
-    readDirectory: (dirPath: string) => Promise<DirectoryContent>;
-    readDirectoryDeep: (dirPath: string, limit?: number, level?: number) => Promise<DirectoryContentDeep | DirectoryContent>;
-    createDirectory: (dirPath: string) => Promise<string>;
-    deleteFileOrDirectory: (dirPath: string) => Promise<void>;
+    readDir: (dirPath: string, ignore?: string[]) => Promise<string[]>;
+    readDirDeep: (dirPath: string, ignore?: string[]) => Promise<string[]>;
+    createDir: (dirPath: string) => Promise<string>;
+    deleteFileOrDir: (dirPath: string) => Promise<void>;
 }
