be0d768592c40fbd96fcfb5466f18467f6a1b966
Timothy Rehenel <timothy.rehenel@gmail.com>
2025-04-12 22:37:37 +0300

    feat: fixed file extension parsing, added hashes in file class

--- src/storage/file.ts
+++ src/storage/file.ts
@@ -2,11 +2,12 @@ import fs from "fs";
 import path from "node:path";
 import {FileHandle} from "fs/promises";
 import {IFile} from "./types.js";
+import {createHash} from "crypto";
 
 export class File implements IFile {
     path: string;
     name: string;
-    extension: string;
+    extension: string = "";
     fullPath: string; // path + name + extension
 
     constructor(fullPath: string) {
@@ -16,8 +17,11 @@ export class File implements IFile {
         if (!fileName) {
             throw new Error("Unable to parse file name")
         }
-        this.name = fileName.split(".").slice(0, -1).join("");
-        this.extension = fileName?.split(".").pop() ?? "";
+        this.name = fileName;
+        if (fileName.split(".").length > 1) {
+            this.name = fileName.split(".").slice(0, -1).join(".");
+            this.extension = fileName.split(".").slice(-1).join("");
+        }
         this.path = parts.join(path.sep);
     }
 
@@ -43,5 +47,27 @@ export class File implements IFile {
         } catch (err: any) {
             throw new Error(`Failed to open file handle: ${err.message}`);
         }
+    }
+
+    async getDataHash(algo: string = "sha256"): Promise<string> {
+        const hash = createHash(algo);
+        const bufferSize = 64 * 1024;
+        const buffer = Buffer.allocUnsafe(bufferSize);
+        const fileHandle = await this.getHandle();
+
+        try {
+            let position = 0;
+            while (true) {
+                const {bytesRead} = await fileHandle.read(buffer, 0, bufferSize, position);
+                if (bytesRead === 0) break;
+
+                hash.update(buffer.subarray(0, bytesRead));
+                position += bytesRead;
+            }
+        } finally {
+            await fileHandle.close();
+        }
+
+        return hash.digest("hex");
     }
 }
